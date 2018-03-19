29055aeddcf30ccc4cfe52447cd684a17b1fdd14
Amanda Wray <amandawray2010@gmail.com>
2018-03-19 15:04:02 -0600

    finished read/write codes

--- my-files.js
+++ my-files.js
@@ -1,27 +1,79 @@
-const {red, yellow} =  require("chalk")
-const path = require("path")
-const fs = require("fs")
+const { red, yellow, cyan } = require('chalk')
+const path = require('path')
+const fs = require('fs')
 
+const filename = path.join(__dirname, 'test.txt')
 
-const filename = path.join(__dirname, "test.txt")
 // console.log(yellow(filename))
-//
-// fs.readdir(__dirname, (err, files) =>{
-//   console.log(files)
+
+// fs.readdir(__dirname, (err, files) => {
+//   if (err) {
+//     console.log(red(err))
+//   }
+//   else {
+//     console.log(files)
+//   }
 // })
+// console.log(yellow('Hello!'))
+
+// fs.readFile(filename, 'utf8', (err, data) => {
+//   if (err) {
+//     return console.log(err)
+//   }
 //
-// console.log(yellow("hello!"))
+//   const lines = data
+//     .split('\n')
+//     .filter(x => x.length > 0)
+//     .map((el, idx) => `${idx}: ${el}`)
+//
+//   console.log(lines)
+// })
 
+// try {
+//   const syncContents = fs.readFileSync('klsjdflksdjflkdsjfldksfj', 'utf8')
+//   console.log(cyan(syncContents))
+// }
+// catch (err) {
+//   console.log(red(err))
+// }
 
-fs.readFile(filename,"utf8", (err, data) => {
-  if(err){
-    return console.log(red(err))
-  }
-  const lines = data
-  .split('\n')
-  .filter(x => x.length > 0)
-  .map((el, idx) => `${idx}: ${el}`)
+// fs.unlink(filename, (err) => {
+//   if (err) {
+//     console.log(red(err))
+//   }
+//   else {
+//     console.log('file has been anhilated')
+//   }
+// })
 
-  console.log(lines)
+function addFruit(type, count, filename='things.csv') {
+  fs.readFile(filename, 'utf8', (err, data) => {
+    if (err) {
+      console.log(red(err))
+    }
+    else {
+      const lines = data
+        .split('\n')
+        .filter(x => x.length > 0)
+        .map(x => x.split(','))
 
-})
+      const fruits = {}
+      lines.forEach(x => fruits[x[0]] = x[1])
+      fruits[type] = count
+
+      const newLines = []
+      Object.keys(fruits).forEach(key => {
+        newLines.push(`${key},${fruits[key]}`)
+      })
+
+      const writeMe = newLines.join('\n')
+      fs.writeFile(filename, writeMe, 'utf8', (err) => {
+        if (err) {
+          console.log(err)
+        }
+      })
+    }
+  })
+}
+
+addFruit('Apples', 210)
