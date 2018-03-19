f07abfd2ecf164594274491974b3e895798e8b0e
Amanda Wray <amandawray2010@gmail.com>
2018-03-19 14:18:30 -0600

    reads a file

--- my-files.js
+++ my-files.js
@@ -3,11 +3,22 @@ const path = require("path")
 const fs = require("fs")
 
 
-// const filename = path.join(__dirname, "test.txt")
+const filename = path.join(__dirname, "test.txt")
 // console.log(yellow(filename))
+//
+// fs.readdir(__dirname, (err, files) =>{
+//   console.log(files)
+// })
+//
+// console.log(yellow("hello!"))
 
-fs.readdir(__dirname, (err, files) =>{
-  console.log(files)
+
+fs.readFile(filename,"utf8", (err, data) => {
+  if(err){
+    return console.log(red(err))
+  }
+  const lines = data.split('\n').filter(x => x.length > 0)
+  
+  console.log(lines)
+
 })
-
-console.log(yellow("hello!"))
